2e45a6325a8a41ac6857b9db330ea54c12ad896e
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-05 17:01:47 -0400

    Extension integrates with maps

--- Extension/IntentHandler.swift
+++ Extension/IntentHandler.swift
@@ -9,10 +9,20 @@
 import Intents
 import UIKit
 
-class INIntentHandler: INExtension, INRidesharingDomainHandling {
+class IntentHandler: INExtension, INRidesharingDomainHandling {
     
     func handle(listRideOptions intent: INListRideOptionsIntent, completion: @escaping (INListRideOptionsIntentResponse) -> Void) {
         
+        let result = INListRideOptionsIntentResponse(code: .success, userActivity: nil)
+        let mini = INRideOption(name: "Mini Cooper", estimatedPickupDate: Date(timeIntervalSinceNow: 1000))
+        let accord = INRideOption(name: "Honda Accord", estimatedPickupDate: Date(timeIntervalSinceNow: 800))
+        let ferrari = INRideOption(name: "Ferrari F430", estimatedPickupDate: Date(timeIntervalSinceNow: 300))
+        ferrari.disclaimerMessage = "This is bad for the environment, but you will definitely get some action!!"
+        
+        result.expirationDate = Date(timeIntervalSinceNow: 3600)
+        result.rideOptions = [mini, accord, ferrari]
+        
+        completion(result)
     }
     
     func handle(requestRide intent: INRequestRideIntent, completion: @escaping (INRequestRideIntentResponse) -> Void) {
